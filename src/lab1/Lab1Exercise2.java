757692aa856690cdf2302d83f4a010d0389fe096
Kalyani Dhavale <kalyanidhavale00@gmail.com>
2021-10-20 15:18:14 +0530

    initial commit

--- src/lab1/Lab1Exercise2.java
+++ src/lab1/Lab1Exercise2.java
@@ -1,9 +1,30 @@
 package lab1;
 
+import java.util.Scanner;
+
 public class Lab1Exercise2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	Scanner sc=new Scanner(System.in);
+	System.out.println("colours:");
+	System.out.println("1.Red \n2.Yellow\n2.Green");
+	System.out.println("Enter your choice:");
+	int ch=sc.nextInt();
+	switch(ch)
+	{
+	case 1:
+		 System.out.println("Stop");
+		 break;
+	case 2:
+		  System.out.println("Reday");
+		  break;
+	case 3:
+		System.out.println("Go");
+		break;
+	default:
+		System.out.println("Please enter valid number");
+	}
+	
 
 	}
 
